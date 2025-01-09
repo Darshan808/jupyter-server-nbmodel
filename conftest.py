5a578c03b4f1cc7ffd75033843a41e515b9dfef6
Darshan808 <pranishpoudel10@gmail.com>
2025-01-09 23:31:17 +0545

    try_1

--- conftest.py
+++ conftest.py
@@ -6,8 +6,3 @@ pytest_plugins = (
     "jupyter_server_fileid.pytest_plugin",
     "jupyter_server.pytest_plugin"
  )
-
-@pytest.fixture
-def jp_server_config(jp_server_config):
-    jp_server_config["ServerApp"]["jpserver_extensions"]["jupyter_server_nbmodel"] = True
-    return jp_server_config
