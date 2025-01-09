ac1c1c3864ba990436ac5dbdd39cc80b549a812e
Darshan808 <pranishpoudel10@gmail.com>
2025-01-09 23:56:53 +0545

    try_5

--- conftest.py
+++ conftest.py
@@ -10,22 +10,6 @@ pytest_plugins = [
 
 @pytest.fixture
 def jp_server_config(jp_root_dir,jp_server_config):
-    return {
-        "ServerApp": {
-            "jpserver_extensions": {
-                "jupyter_server_ydoc": True,
-                "jupyter_server_nbmodel": True,
-                "jupyter_server_fileid": True, 
-            },
-            "SQLiteYStore": {"db_path": str(jp_root_dir.joinpath(".rtc_test.db"))},
-            "BaseFileIdManager": {
-                "root_dir": str(jp_root_dir),
-                "db_path": str(jp_root_dir.joinpath(".fid_test.db")),
-                "db_journal_mode": "OFF",
-            },
-            "YDocExtension": {"document_save_delay": 1},
-            "IdentityProvider": {"token": ""},
-            "password":"",
-            "disable_check_xsrf": True,
-        },
-    }
+    print("\n\n\njp_server_config\n",jp_server_config,"\n\n\n")
+    print("jp_root_dir\n",jp_root_dir,"\n\n\n")
+    return jp_server_config
