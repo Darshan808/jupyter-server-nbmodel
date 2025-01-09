a70ac10a7be603d58a12693f0b9733475d93051f
Darshan808 <pranishpoudel10@gmail.com>
2025-01-09 20:51:18 +0545

    try-fileid.pytest_plugin

--- conftest.py
+++ conftest.py
@@ -2,7 +2,8 @@ import pytest
 
 pytest_plugins = (
     "pytest_jupyter.jupyter_server",
-    "jupyter_server_ydoc.pytest_plugin"
+    "jupyter_server_ydoc.pytest_plugin",
+    "jupyter_server_fileid.pytest_plugin"
  )
 
 
