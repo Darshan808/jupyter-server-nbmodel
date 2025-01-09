f9adcc8f2d547f5338d251a900847a549622f8aa
Darshan808 <pranishpoudel10@gmail.com>
2025-01-09 23:17:16 +0545

    default config

--- conftest.py
+++ conftest.py
@@ -1,7 +1,13 @@
 import pytest
 
-pytest_plugins = [
-    "jupyter_server.pytest_plugin",
-    "jupyter_server_fileid.pytest_plugin",
+pytest_plugins = (
+    "pytest_jupyter.jupyter_server",
     "jupyter_server_ydoc.pytest_plugin",
-]
+    "jupyter_server_fileid.pytest_plugin"
+    "jupyter_server.pytest_plugin",
+ )
+
+@pytest.fixture
+def jp_server_config(jp_server_config):
+    jp_server_config["ServerApp"]["jpserver_extensions"]["jupyter_server_nbmodel"] = True
+    return jp_server_config
