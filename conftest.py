ce81d92a65ecad5dca0deabdbca4a71df8fae683
Darshan808 <pranishpoudel10@gmail.com>
2025-01-09 23:20:10 +0545

    missing comma 2

--- conftest.py
+++ conftest.py
@@ -3,8 +3,8 @@ import pytest
 pytest_plugins = (
     "pytest_jupyter.jupyter_server",
     "jupyter_server_ydoc.pytest_plugin",
-    "jupyter_server_fileid.pytest_plugin"
-    "jupyter_server.pytest_plugin",
+    "jupyter_server_fileid.pytest_plugin",
+    "jupyter_server.pytest_plugin"
  )
 
 @pytest.fixture
