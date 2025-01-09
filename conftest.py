393874aa97fddedac423680b69c09cd78ea7fc0c
Darshan808 <pranishpoudel10@gmail.com>
2025-01-09 22:43:35 +0545

    add new pytest_plugin

--- conftest.py
+++ conftest.py
@@ -4,6 +4,7 @@ pytest_plugins = (
     "pytest_jupyter.jupyter_server",
     "jupyter_server_ydoc.pytest_plugin",
     "jupyter_server_fileid.pytest_plugin"
+    "jupyter_server.pytest_plugin",
  )
 
 
