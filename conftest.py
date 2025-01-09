cdbface0ef490c6c30c38c712cefebfb542a3c3d
Darshan808 <pranishpoudel10@gmail.com>
2025-01-09 23:01:27 +0545

    same as collaborator

--- conftest.py
+++ conftest.py
@@ -1,24 +1,7 @@
 import pytest
 
-pytest_plugins = (
-    "pytest_jupyter.jupyter_server",
-    "jupyter_server_ydoc.pytest_plugin",
-    "jupyter_server_fileid.pytest_plugin",
+pytest_plugins = [
     "jupyter_server.pytest_plugin",
- )
-
-
-@pytest.fixture
-def jp_server_config(jp_root_dir,jp_server_config):
-    return {
-        "ServerApp": {
-            "jpserver_extensions": {
-                "jupyter_server_ydoc": True,
-                "jupyter_server_nbmodel": True,
-                "jupyter_server_fileid": True, 
-            },
-            "YDocExtension": {"document_save_delay": 1},
-            "IdentityProvider": {"token": ""},
-            "disable_check_xsrf": True,
-        },
-    }
+    "jupyter_server_fileid.pytest_plugin",
+    "jupyter_server_ydoc.pytest_plugin",
+]
