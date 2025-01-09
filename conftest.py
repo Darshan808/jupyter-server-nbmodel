22b83509ac7a0298bf82182db115fabd4a10a724
Darshan808 <pranishpoudel10@gmail.com>
2025-01-09 22:53:42 +0545

    reduce config

--- conftest.py
+++ conftest.py
@@ -17,12 +17,6 @@ def jp_server_config(jp_root_dir,jp_server_config):
                 "jupyter_server_nbmodel": True,
                 "jupyter_server_fileid": True, 
             },
-            "SQLiteYStore": {"db_path": str(jp_root_dir.joinpath(".rtc_test.db"))},
-            "BaseFileIdManager": {
-                "root_dir": str(jp_root_dir),
-                "db_path": str(jp_root_dir.joinpath(".fid_test.db")),
-                "db_journal_mode": "OFF",
-            },
             "YDocExtension": {"document_save_delay": 1},
             "IdentityProvider": {"token": ""},
             "disable_check_xsrf": True,
