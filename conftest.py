c84bd1241538c58b4971cc62f4ee904c426aef7b
Darshan808 <pranishpoudel10@gmail.com>
2025-01-10 07:37:00 +0545

    added-server-plugin

--- conftest.py
+++ conftest.py
@@ -2,7 +2,7 @@ import pytest
 
 
 pytest_plugins = [
-    # "pytest_jupyter.jupyter_server",
+    "pytest_jupyter.jupyter_server",
     "jupyter_server.pytest_plugin",
     "jupyter_server_fileid.pytest_plugin",
     "jupyter_server_ydoc.pytest_plugin"
